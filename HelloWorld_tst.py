919682f9d2ef78315c07906e01f2d29947db8f95
santhosh4git <suryasanthosh@gmail.com>
2019-01-20 15:18:35 +0530

    Update HelloWorld_tst.py

--- HelloWorld_tst.py
+++ HelloWorld_tst.py
@@ -4,7 +4,7 @@ from HelloWorld import HelloWorld
 class MyTestCase(unittest.TestCase):
   def test_default_greeting_set(self):
     hw = HelloWorld()
-    self.assertEqual(hw.message, 'Hello world!')
+    self.assertEqual(hw.message, 'Hello world! Updated.!')
 
 if __name__ == '__main__':
   unittest.main()
